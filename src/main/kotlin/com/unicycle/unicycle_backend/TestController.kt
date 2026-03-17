070529740258361bf6e9fa86313f90c51bf69599
Omer Gafar <gafaro@rpi.edu>
2026-03-17 13:13:22 -0400

    chore: clean up test controller package and response"

--- src/main/kotlin/com/unicycle/unicycle_backend/TestController.kt
+++ src/main/kotlin/com/unicycle/unicycle_backend/TestController.kt
@@ -1,10 +1,13 @@
-package com.example.unicycle
+package com.unicycle.unicycle_backend
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class TestController {
+
     @GetMapping("/test")
-    fun test() = "Docker is working!"
+    fun test(): Map<String, String> {
+        return mapOf("message" to "Backend is running")
+    }
 }
